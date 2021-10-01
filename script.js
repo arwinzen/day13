114e7508eb6450326cf0427d01ca3225b383e103
rw1n <arwingoo90@gmail.com>
2021-10-01 09:28:45 +0800

    added comments to script.js

--- script.js
+++ script.js
@@ -13,47 +13,62 @@ const firebaseConfig = {
     firebase.initializeApp(firebaseConfig);
     console.log(firebase);
 
+// declare a variable for the credentials container
 const credentials = document.querySelector('.credentials');
 
-
+// add a submit event listener for credentials
 credentials.addEventListener('submit', handleLogin);
 
+// logic to handleLogin
 function handleLogin(e){
+    // prevent page from refereshing during submission
     e.preventDefault();
 
-    // get data from form 
-    // let userid= e.target.elements.userid.value;
+    // get data from from form
+    // create a variable in sessionStorage
     sessionStorage.setItem('userid', e.target.elements.userid.value);
+    // assign a local variable and get the value from sessionStorage
     let userid = sessionStorage.getItem('userid');
+    // get password from the password input
     let password = e.target.elements.password.value;
+    // set target to the form element
     let target = e.target;
+    // initialise userFound to false
     let userFound = false;
 
-    // if submit belongs to login button 
+    // check which form is being targeted (login vs signup)
+    // logic for login form submission
     if (target && target.matches('.login-form')){
         console.log('matches login form');
-        // check if user exists
+        // load list of users from users table in firebase db
         firebase.database().ref('chat').child('users').on('child_added', function(snapshot){
             // console.log(snapshot);
             console.log(snapshot.val().userid);
+            // declare variables for userid and password from data retrieved
             let nameVal = snapshot.val().userid;
             let pwdVal = snapshot.val().password;
 
+            // user validation logic
             if (userid == nameVal && password == pwdVal){
-                // reset form fields
                 console.log(userFound);
+                // set userFound to true if user and password matches
                 userFound = true;
+                // reset form
                 e.target.reset();
+                // redirect to chat page
                 window.location.href = "chat.html";
             } else {
+                // set userFound to false
                 userFound = false;
             }
         })
+        // wait for user validation to complete before alerting for invalid credentials
         setTimeout(function(){
             if (userFound == false){
                 alert('Invalid credentials');
             }
         }, 800);
+      // logic for signup form submission
     } else if (target && target.matches('.signup-form')){
         console.log('matches signup form');
         // push user credentials to the users table in firebase
@@ -63,12 +78,14 @@ function handleLogin(e){
         });
         alert(`Created account for ${userid}. Logging you in.`);
         e.target.reset();
+        // wait for user to be created in firebase before redirecting to chat page
         setTimeout(function(){
             window.location.href = "chat.html";
         }, 1000);
     }
 }
 
+// add a click listener for the forms
 credentials.addEventListener('click', displayForm);
 
 function displayForm(e){
